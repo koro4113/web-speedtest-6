6d287cd32227387e51d3445558a1604b312afa5b
koro4113 <66620396+koro4113@users.noreply.github.com>
2020-07-04 17:27:33 +0300

    sp

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -7,11 +7,29 @@
  * - name إن استقبلت اسم أو "Hello <name>" تطبع
  */
 
-function greet(name) {}
+function greet(name) {
+let message = "hello";
+if(name){  
+  message += name;
+
+    console.log(message);
+
+}
 
 /*
  * isOdd(n) اسم الدالة
  * - n تستقبل رقم
  * - عدا ذلك false إن كان الرقم فردياً وترجع true ترجع القيمة
  */
-function isOdd(n) {}
+
+ function isOdd(n) {
+ if(n % 2 == 1){
+return true;
+ }  
+else{
+ return false;
+}
+
+}
+
+console.log(isOdd(4));
